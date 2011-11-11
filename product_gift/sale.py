c22d7819c6b5d2f9065552629015ff113d12e955
sebastien beau <sebastien.beau@akretion.com.br>
2011-11-11 19:57:15 +0100

    [IMP] improve port on V6

--- product_gift/sale.py
+++ product_gift/sale.py
@@ -23,40 +23,27 @@ from osv import osv, fields
 import netsvc
 
 
-class stock_picking(osv.osv):
-    _inherit = "stock.picking"
-
+class sale_order(osv.osv):
+    
+    _inherit = "sale.order"
+    
     _columns = {
         'gift_message': fields.text('Gift Message'),
+
     }
 
-    def create(self, cr, uid, vals, context=None):
-        print 'create picking', vals.get('sale_id', False) and not 'gift_message' in vals
-        if vals.get('sale_id', False) and not 'gift_message' in vals:
-            order = self.pool.get('sale.order').browse(cr, uid, vals['sale_id'], context=context)
-            print 'order.name', order.name, order.gift_message
-            vals.update({'gift_message' : order.gift_message})
-            print 'vals', vals
-        return super(stock_picking, self).create(cr, uid, vals, context=context)
+sale_order()
 
-stock_picking()
-
-
-class stock_move(osv.osv):
-    _inherit = "stock.move"
-
+class sale_order_line(osv.osv):
+    
+    _inherit = "sale.order.line"
+    
     _columns = {
         'gift_message': fields.text('Gift Message'),
-        'need_gift_wrap': fields.boolean('Need Gift Wrap'),
+        'need_gift_wrap': fields.boolean('Add Gift Wrap'),
+
     }
 
-    def create(self, cr, uid, vals, context=None):
-        print 'create move', vals.get('sale_line_id', False) and not ('gift_message' in vals and 'need_gift_wrap' in vals)
-        if vals.get('sale_line_id', False) and not ('gift_message' in vals and 'need_gift_wrap' in vals):
-            line = self.pool.get('sale.order.line').browse(cr, uid, vals['sale_line_id'], context=context)
-            vals.update({'gift_message' : line.gift_message, 'need_gift_wrap': line.need_gift_wrap})
-            print 'vals', vals
-        return super(stock_move, self).create(cr, uid, vals, context=context)
+sale_order_line()
 
-
-stock_move()
+ 
