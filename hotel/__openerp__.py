6690759115ea09f8593f05ade72616ad56870f66
Serpent Consulting Services <support@serpentcs.com>
2011-12-28 03:04:39 +0530

    [FIX/IMP] Hotel : Made the module compatible to 6.0+

--- hotel/__openerp__.py
+++ hotel/__openerp__.py
@@ -19,36 +19,38 @@
 #
 ##############################################################################
 
-from osv import osv,fields
+{
+    "name" : "Hotel Management",
+    "version" : "1.0",
+    "author" : "Tiny",
+    "category" : "Generic Modules/Hotel Management",
+    "description": """
+    Module for Hotel/Resort/Property management. You can manage:
+    * Configure Property
+    * Hotel Configuration
+    * Check In, Check out
+    * Manage Folio
+    * Payment
 
-class folio_report_wizard(osv.osv_memory):
-    
-    _name = 'folio.report.wizard'
-    
-    _rec_name = 'date_start'
-    _columns = {
-                'date_start':fields.datetime('Start Date'),
-                'date_end':fields.datetime('End Date')
-                }
-    
-    def print_report(self,cr,uid,ids,context=None):
-        
-        if context is None:
-            context = {}
-            
-        datas = {'ids': context.get('active_ids', [])}
-        res = self.read(cr, uid, ids, context=context)
-        res = res and res[0] or {}
-        datas['form'] = res
+    Different reports are also provided, mainly for hotel statistics.
+    """,
+    "depends" : ["base","product","sale"],
+    "init_xml" : [],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
 
-        return {
-            'type': 'ir.actions.report.xml',
-            'report_name': 'folio.total',
-            'datas': datas,
-        }
+                    "hotel_view.xml",
+                    "hotel_data.xml",
+                    "hotel_folio_workflow.xml",
+                    "hotel_report.xml",
+                    "wizard/hotel_wizard.xml",
+                    "security/hotel_security.xml",
+                    "security/ir.model.access.csv",
 
-        
-folio_report_wizard()
+    ],
+    "active": False,
+    "installable": True
+}
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
