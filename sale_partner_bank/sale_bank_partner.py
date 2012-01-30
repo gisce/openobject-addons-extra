2ed1029c0ce0609779df7698593036a16034a36c
Alexis de Lattre <alexis@via.ecp.fr>
2012-01-30 16:49:03 +0100

    Add "sale_partner_bank" module.
    This modules adds a 'Bank account' field on partners and sale orders. This field is copied from partner to sale order and then from sale order to customer invoice.
    
    This module is similar to the 'sale_payment' module ; the main difference is that it doesn't have any extra-dependancy i.e. it doesn't depend on the 'account_payment_extension' module.

---
+++ sale_partner_bank/sale_bank_partner.py
@@ -0,0 +1,72 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Sale partner bank module for OpenERP
+#    Copyright (C) 2012 Akretion (http://www.akretion.com). All Rights Reserved
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class res_partner(osv.osv):
+    _inherit = 'res.partner'
+
+    _columns = {
+        'partner_bank_id': fields.many2one('res.partner.bank','Bank account', select=True, help='Select the bank account on which the customer should pay. This field will be copied to the sale order and from there to the customer invoice.'),
+        }
+
+
+class sale_order(osv.osv):
+    _inherit = "sale.order"
+
+    _columns = {
+        'partner_bank_id': fields.many2one('res.partner.bank','Bank account', select=True, help='Select the bank account on which the customer should pay. This field is copied from the partner form and will be copied to the customer invoice.'),
+        }
+
+    def onchange_partner_id(self, cr, uid, ids, part):
+        result = super(sale_order, self).onchange_partner_id(cr, uid, ids, part)
+        if part:
+            partner = self.pool.get('res.partner').read(cr, uid, part, ['partner_bank_id'])
+            if partner:
+                result['value'].update({'partner_bank_id': partner['partner_bank_id']})
+        return result
+
+    def action_invoice_create(self, cr, uid, ids, grouped=False, states=['confirmed', 'done', 'exception'], date_inv = False, context=None):
+# We must return a unique ID !!!
+        invoice_id = super(sale_order, self).action_invoice_create(cr, uid, ids, grouped=grouped, states=states, date_inv=date_inv, context=context)
+        cur_sale_order = self.pool.get('sale.order').browse(cr, uid, ids[0], context=context)
+        self.pool.get('account.invoice').write(cr, uid, invoice_id, {
+                'partner_bank_id': cur_sale_order.partner_bank_id.id
+            }, context=context)
+        return invoice_id
+
+
+class stock_picking(osv.osv):
+    _inherit = "stock.picking"
+
+    def action_invoice_create(self, cr, uid, ids, journal_id=False,
+        group=False, type='out_invoice', context=None):
+        create_ids = super(stock_picking, self).action_invoice_create(cr, uid, ids,
+            journal_id=journal_id, group=group, type=type, context=context)
+        for picking_id in create_ids:
+            picking = self.pool.get('stock.picking').browse(cr, uid, picking_id, context=context)
+            if picking.sale_id:
+                self.pool.get('account.invoice').write(cr, uid, create_ids[picking_id], {
+                    'partner_bank_id': picking.sale_id.partner_bank_id.id,
+                }, context=context)
+        return create_ids
+
